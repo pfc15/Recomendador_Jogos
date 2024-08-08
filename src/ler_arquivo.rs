118f49aceb302179943341ac9515df740258886a
Pedro Cruz <pedrofcrux@gmail.com>
2024-08-07 21:12:49 -0300

    leitura de arquivo completa

--- src/ler_arquivo.rs
+++ src/ler_arquivo.rs
@@ -5,15 +5,42 @@ pub fn da_oi() {
     println!("oi :)")
 }
 
-pub fn leitura(caminho: String){
+pub fn leitura(caminho: String) -> Vec<(String, i32)>{
     
     let contents = fs::read_to_string(caminho)
         .expect("deveria ter lido o arquivo");
-    
-    for  c in contents.chars(){
-        print!("{c}")
+    let mut nome_bool = true;
+    let mut primeira = true;
+    let mut nome = String::new();
+    let mut horas_jogadas = String::new();
+    let mut array:Vec<(String, i32)> = Vec::new(); 
+    for  c in contents.chars() {
+
+        if c == ','{
+            nome_bool = !nome_bool;
+        }else if c == '\n'{
+            if primeira{
+                primeira = false;
+            } else{
+                let num_horas_jogadas: i32 = horas_jogadas.parse().expect("");
+                array.push((nome, num_horas_jogadas));
+            }
+            nome_bool = !nome_bool;
+            nome = String::new();
+            horas_jogadas = String::new();
+            
+        }
+        else{
+            if nome_bool{
+                nome.push(c);
+            } else {
+                horas_jogadas.push(c);
+            }
+            println!("nome: {nome}; horas jogadas: {horas_jogadas}");
+        }
     }
 
     println!("texto: {contents}");
 
+    return array;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod ler_arquivo;
 
 fn main() {
-    ler_arquivo::leitura(String::from("/home/pfc15/Documents/2024.1/pa/steam_game/pedro.csv"));
-
+    let mut array:Vec<(String, i32)> = ler_arquivo::leitura(String::from("/home/pfc15/Documents/2024.1/pa/steam_game/pedro.csv"));
+    for (a, b) in array{
+        println!("nome: {a}; horas jogadas: {b}");
+    }
 }
 
 
